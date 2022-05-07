019ed08a7d978981d6955c4b62dda277480d509b
Nolusindiso Hleko <melanated_butterfly@Badass-MacBook-Pro.local>
2022-05-07 22:40:21 +0900

    added other pages

--- portfolio/views.py
+++ portfolio/views.py
@@ -11,7 +11,17 @@ views = Blueprint("views",__name__)
  #home page view
 @views.route('/')
 @views.route('/home')
+@views.route('/portfolio')
 def home():
     return render_template('portfolio.html')
+
+
+@views.route('/workflow')
+def workflow():
+    return render_template('workflow.html')
+
+@views.route('/gallery-workflow')
+def galleryworkflow():
+    return render_template('gallery-workflow.html')
 
 
